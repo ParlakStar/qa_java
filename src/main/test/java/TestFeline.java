a8327348b753c04085460bdf15e1c54b8067ecd2
Alexandr Parlakyan <alex_man2@rambler.ru>
2023-05-14 21:43:42 +0200

    First2 commit

--- src/main/test/java/TestFeline.java
+++ src/main/test/java/TestFeline.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestFeline {
